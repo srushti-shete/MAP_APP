ee3e52ef331f1f52d84d76708c31c01b7fb56f5c
Srushti Shete <shetesrushti11@gmail.com>
2023-07-04 23:45:40 +0530

    Added Register & Login feature

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log("MongoDB Connected")
     }).catch(err=>console.log(err));
 
+app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
 app.listen(8800, () => {
